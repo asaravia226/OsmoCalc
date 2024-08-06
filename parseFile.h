24d939a74de9207f2cf0622276b452d77e414112
asaravia226 <asaravia@USCAPC1VLTJ7W.bos1.vrtx.com>
2024-08-06 12:04:47 -0700

    added more code, stopped at pushing structs to vector

--- parseFile.h
+++ parseFile.h
@@ -4,16 +4,30 @@
 #include <fstream>
 #include <vector> 
 #include <string>
+#include <sstream> 
+
 // This file will open the necessary txt file and catch any file errors
 // It will also modify the original txt file if prompted by the user. 
 
 if(!(std::fstream file("ingredients.txt"))) {
 	throw std::runtime_error( "ingredients.txt not found! Check directory to see if file exists.");  
 	else {
-		while (!file.eof()){
-			std::string line; 
-
+		std::vector<ingredient> list; 
+		std::string line; 
+		std::string name; 
+		std::temp_ion_dissoc; 
+		std::ifstream fileStream; 
 		
+		while (std::getline(fileStream, line)) {
+			std::stringstream ss(line);
+			std::getline(ss, name, ',');
+			list.
+			
+				
+			std::getline(ss,temp_ion_dissoc, '\n'); 		
+			try {std::stoi(temp_ion_dissoc);}
+			catch(std::exception& e){std::cout << "No Ion Dissociation. Update 'ingredients.txt' file for ingredient: " << name << "\n";}
+			
 		}
 	}
 }	
